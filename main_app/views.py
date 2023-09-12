fd929bafef89d8594bb1c5b4bd535f1eb7aab56b
marceloiuli <marceloiuli26@gmail.com>
2023-09-12 16:46:28 -0700

    Added new finches to db Lab pt 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-finches = [
-    {'name': 'Waynebow', 'breed': 'Gouldian Finch', 'description': 'rainbow colored buddy', 'age': 10},
-    {'name': 'Finkachu', 'breed': 'American Goldfinch', 'description': 'bright yellow and very energetic', 'age': 6},
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -13,6 +9,13 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
     })
